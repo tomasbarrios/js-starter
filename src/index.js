d2f4c95515c374e3dfe965143186f8cd85781e8d
Tomas Barrios <tomas.barrios@globant.com>
2018-08-18 12:06:03 -0300

    uses webpack with config file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
     let element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hellooo', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hellooo with config!', 'webpack'], ' ');
   
     return element;
   }
